9f358b80e1a3037508ead783a4416c445c2c0ae6
busatoju <busatoju@gmail.com>
2021-03-08 15:59:20 -0300

    Criado o modelo de contato

---
+++ lib/models/contact.dart
@@ -0,0 +1,12 @@
+class Contact{
+
+  String name;
+  int numberAccount;
+
+  Contact(this.name, this.numberAccount);
+
+  @override
+  String toString() {
+    return 'Contact{name: $name, numberAccount: $numberAccount}';
+  }
+}
