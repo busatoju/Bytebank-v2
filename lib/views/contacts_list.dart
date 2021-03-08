601cb1b9877fc3fc6bc1276ff30f5782463c9bac
busatoju <busatoju@gmail.com>
2021-03-08 13:25:59 -0300

    Criação da tela de lista de contatos e chamado para tela de formulário de criação de contato

--- lib/views/contacts_list.dart
+++ lib/views/contacts_list.dart
@@ -1,14 +1,29 @@
+import 'package:bytebank_version_two/views/contact_form.dart';
 import 'package:flutter/material.dart';
 
-class Contact extends StatelessWidget {
+class ContactsList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Contacts'),),
-      body: FloatingActionButton(
+      body: ListView(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Card(
+              child: ListTile(
+                title: Text('Juliana', style: TextStyle(fontSize: 24.0),),
+                subtitle: Text('27665843', style: TextStyle(fontSize: 16.0),),
+              ),
+            ),
+          ),
+        ],
+      ),
+
+      floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add),
         onPressed: (){
-
+          Navigator.of(context).push(MaterialPageRoute(builder: (context) => ContactForm()));
         }
       ),
     );
