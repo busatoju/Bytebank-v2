8b2410e47b49d6625366c1e4583eb7bdf3df5938
busatoju <busatoju@gmail.com>
2021-03-10 08:40:50 -0300

    Implementada tela de carregamento

--- lib/views/contacts_list.dart
+++ lib/views/contacts_list.dart
@@ -1,30 +1,85 @@
+import 'package:bytebank_version_two/database/app_database.dart';
+import 'package:bytebank_version_two/models/contact.dart';
 import 'package:bytebank_version_two/views/contact_form.dart';
 import 'package:flutter/material.dart';
 
 class ContactsList extends StatelessWidget {
+  List<Contact> contacts = List();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Contacts'),),
-      body: ListView(
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Card(
-              child: ListTile(
-                title: Text('Juliana', style: TextStyle(fontSize: 24.0),),
-                subtitle: Text('27665843', style: TextStyle(fontSize: 16.0),),
-              ),
-            ),
-          ),
-        ],
+      appBar: AppBar(
+        title: Text('Contacts'),
       ),
+      body: FutureBuilder<List<Contact>>(
+        initialData: List(),
+        future:  findAll(),
+        builder: (context, snapshot) {
+          switch(snapshot.connectionState){
 
+            case ConnectionState.none:
+              break;
+            case ConnectionState.waiting:
+              return Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    CircularProgressIndicator(),
+                    Text('Loading')
+                  ],
+                ),
+              );
+              break;
+            case ConnectionState.active:
+              break;
+            case ConnectionState.done:
+              final List<Contact> contacts = snapshot.data;
+              return ListView.builder(
+                itemBuilder: (BuildContext context, int index) {
+                  final Contact contact = contacts[index];
+                  return _ContactItem(contact);
+                },
+                itemCount: contacts.length,
+              );
+              break;
+          }
+          return Text('Unknow Error');
+
+
+
+
+        },
+      ),
       floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.add),
-        onPressed: (){
-          Navigator.of(context).push(MaterialPageRoute(builder: (context) => ContactForm())).then((value) => debugPrint(value.toString()));
-        }
+          child: Icon(Icons.add),
+          onPressed: () {
+            Navigator.of(context)
+                .push(MaterialPageRoute(builder: (context) => ContactForm()))
+                .then((value) => debugPrint(value.toString()));
+          }),
+    );
+  }
+}
+
+class _ContactItem extends StatelessWidget {
+  final Contact contact;
+
+  _ContactItem(this.contact);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: ListTile(
+        title: Text(
+          contact.name,
+          style: TextStyle(fontSize: 24.0),
+        ),
+        subtitle: Text(
+          contact.numberAccount.toString(),
+          style: TextStyle(fontSize: 16.0),
+        ),
       ),
     );
   }
