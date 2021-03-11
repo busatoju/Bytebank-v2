8aac604acabb88e1cca58bc404a1eb75ada6c8c0
busatoju <busatoju@gmail.com>
2021-03-11 08:57:35 -0300

    Refatoração de código

--- lib/database/app_database.dart
+++ lib/database/app_database.dart
@@ -1,46 +1,35 @@
-import 'package:bytebank_version_two/models/contact.dart';
+import 'package:bytebank_version_two/database/contact_dao.dart';
 import 'package:path/path.dart';
 import 'package:sqflite/sqflite.dart';
 
-Future<Database> createDatabase() {
-  return getDatabasesPath().then((dbPath) {
-    final String path = join(dbPath, 'bytebank.db');
-    return openDatabase(
-      path,
-      onCreate: (db, version) {
-        db.execute('CREATE TABLE contacts('
-            'id INTEGER PRIMARY KEY, '
-            'name TEXT,'
-            'account_number INTEGER)');
-      },
-      version: 1,
-      //onDowngrade: onDatabaseDowngradeDelete,
-    );
-  });
-}
-
-Future<int> save(Contact contact) {
-  return createDatabase().then((db) {
-    final Map<String, dynamic> contactMap = Map();
-    contactMap['name'] = contact.name;
-    contactMap['account_number'] = contact.numberAccount;
-    return db.insert('contacts', contactMap);
-  });
-}
-
-Future<List<Contact>> findAll() {
-  return createDatabase().then((db) {
-    return db.query('contacts').then((maps) {
-      final List<Contact> contacts = List();
-      for (Map<String, dynamic> map in maps) {
-        final Contact contact = Contact(
-          map['id'],
-          map['name'],
-          map['account_number'],
+Future<Database> getDatabase() async{
+  final String path = join(await getDatabasesPath(), 'bytebank.db');
+  return openDatabase(
+          path,
+          onCreate: (db, version) {
+            db.execute(ContactDao.tableSql);
+          },
+          version: 1,
+          //onDowngrade: onDatabaseDowngradeDelete,
         );
-        contacts.add(contact);
-      }
-      return contacts;
-    });
-  });
+
+
+  // return getDatabasesPath().then((dbPath) {
+  //   final String path = join(dbPath, 'bytebank.db');
+  //   return openDatabase(
+  //     path,
+  //     onCreate: (db, version) {
+  //       db.execute('CREATE TABLE contacts('
+  //           'id INTEGER PRIMARY KEY, '
+  //           'name TEXT,'
+  //           'account_number INTEGER)');
+  //     },
+  //     version: 1,
+  //     //onDowngrade: onDatabaseDowngradeDelete,
+  //   );
+  // });
 }
+
+
+
+
