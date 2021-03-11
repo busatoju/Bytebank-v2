e91c1738704a1b5afa02be2eb7b794ba7ee02304
busatoju <busatoju@gmail.com>
2021-03-11 08:58:05 -0300

    Adicionando o contact_dao a view

--- lib/views/contact_form.dart
+++ lib/views/contact_form.dart
@@ -1,8 +1,9 @@
-import 'package:bytebank_version_two/database/app_database.dart';
+import 'package:bytebank_version_two/database/contact_dao.dart';
 import 'package:bytebank_version_two/models/contact.dart';
 import 'package:flutter/material.dart';
 
 class ContactForm extends StatefulWidget {
+
 
   @override
   _ContactFormState createState() => _ContactFormState();
@@ -12,6 +13,7 @@ class _ContactFormState extends State<ContactForm> {
   TextEditingController controllerName = TextEditingController();
 
   TextEditingController controllerAccountNumber = TextEditingController();
+  final ContactDao _dao = ContactDao();
 
   @override
   Widget build(BuildContext context) {
@@ -48,7 +50,7 @@ class _ContactFormState extends State<ContactForm> {
                 width: double.maxFinite,
                 child: RaisedButton(child: Text('Create'), onPressed: () {
                 Contact newContact =  Contact(1,controllerName.text, int.tryParse(controllerAccountNumber.text));
-                save(newContact).then((id) => Navigator.pop(context));
+                _dao.save(newContact).then((id) => Navigator.pop(context));
 
                 }),
               ),
